f59feec6e40948cb8317f230fb1db5c966ea3a45
Mohamed68270 <mohamed.ladjimi@epitech.eu>
2022-03-16 20:58:18 +0100

    nav

---
+++ first-react-app/src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quisquam
+        similique, nobis error minima molestias facere atque pariatur dolor
+        culpa voluptatibus repudiandae ex nesciunt veniam unde possimus eius id
+        quod cum incidunt fuga, animi maiores. Fugit aperiam mollitia libero
+        ratione sed voluptatibus enim quibusdam deleniti animi facilis, numquam
+        illum esse saepe quasi laboriosam consectetur minus molestias voluptas
+        eum doloremque ut optio sunt. Dolor ab voluptatum ipsum? Nostrum officia
+        vel dignissimos labore ab molestiae at inventore deleniti perspiciatis
+        totam nihil cum, aliquam nulla odit veniam? Cupiditate nesciunt eveniet
+        iusto natus repellat minus amet ut consequuntur, commodi, neque saepe
+        architecto perferendis autem?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam
+        voluptates a error voluptas sit laboriosam dolor fugiat pariatur,
+        doloribus iste animi placeat est, dicta quasi! Architecto, velit?
+        Blanditiis dolorem labore cumque illum beatae voluptates aperiam dolores
+        iusto, incidunt eos aliquam maxime quas iste dolore praesentium, sint
+        voluptatum quod assumenda corporis architecto expedita accusamus? Sit ex
+        aliquid recusandae exercitationem architecto, fuga, repudiandae soluta
+        enim deserunt cupiditate eveniet eos quas quasi iusto possimus magnam
+        temporibus voluptates, atque tempore eaque provident quibusdam harum ad.
+        Aliquid error iure placeat eius harum quis cumque tempore nisi, ipsam
+        repudiandae veniam deserunt at accusantium illum esse laudantium quaerat
+        itaque rem corrupti consectetur, maxime reprehenderit dolorum
+        distinctio. Eos at accusantium alias fuga repellendus vitae ea maiores!
+        Architecto dolorem eum nulla ex, voluptatum tenetur beatae atque vitae
+        esse. Fugiat saepe ipsa mollitia commodi omnis consequuntur nemo at
+        provident aut. Tenetur necessitatibus iusto dicta incidunt architecto
+        delectus voluptate totam recusandae vero ipsum provident, nulla dolores
+        expedita reiciendis inventore qui consectetur earum iste exercitationem
+        pariatur dolore molestias ut tempora? Necessitatibus officiis aliquam
+        fugit velit expedita numquam sapiente, consectetur recusandae neque
+        atque laudantium voluptates. Asperiores illo ducimus dignissimos, nobis,
+        dolor, repellendus sapiente provident facilis praesentium accusamus
+        consequatur! Amet incidunt in nobis eos?
+      </p>
+    </div>
+  );
+};
+
+export default About;
